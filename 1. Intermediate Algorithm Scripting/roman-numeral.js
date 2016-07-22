df516f5e75b50956b5753908f5beec20428b3b52
Jefferson Nascimento <jefferson.nascimento3@fatec.sp.gov.br>
2016-07-22 15:23:22 -0300

    Refactoring solution: Roman Numeral Converter

--- 1. Intermediate Algorithm Scripting/roman-numeral.js
+++ 1. Intermediate Algorithm Scripting/roman-numeral.js
@@ -1,40 +1,47 @@
 // https://www.freecodecamp.com/challenges/roman-numeral-converter
+function romanNumeral(numeral, value) {
+    this.numeral = numeral;
+    this.value = value;
+}
 
-function convert(num) {
+var romanNumerals = [
+    new romanNumeral("M", 1000), new romanNumeral("CM", 900),
+    new romanNumeral("D", 500), new romanNumeral("CD", 400),
+    new romanNumeral("C", 100), new romanNumeral("XC", 90),
+    new romanNumeral("L", 50), new romanNumeral("XL", 40),
+    new romanNumeral("X", 10), new romanNumeral("IX", 9),
+    new romanNumeral("V", 5), new romanNumeral("IV", 4),
+    new romanNumeral("I", 1)
+];
+
+// function arabicToRoman(arabic) {
+//     var result = "";
+
+//     romanNumerals.forEach(function(roman) {
+//         while (arabic >= roman.value && arabic !== 0) {
+//             result += roman.numeral;
+//             arabic -= roman.value;
+//         }
+//     });
+
+//     return result;
+// }
+
+function convertToRoman(num) {
+    var result = "";
+
     if (num < 1 || num > 3999) {
         return;
     }
 
-    function romanNumeral(_numeral, _value) {
-        this.numeral = _numeral;
-        this.value = _value;
-    }
+    romanNumerals.forEach(function(roman) {
+        while (num >= roman.value && num !== 0) {
+            result += roman.numeral;
+            num -= roman.value;
+        }
+    });
 
-    var romanNumerals = [
-        new romanNumeral("M", 1000), new romanNumeral("CM", 900),
-        new romanNumeral("D", 500), new romanNumeral("CD", 400),
-        new romanNumeral("C", 100), new romanNumeral("XC", 90),
-        new romanNumeral("L", 50), new romanNumeral("XL", 40),
-        new romanNumeral("X", 10), new romanNumeral("IX", 9),
-        new romanNumeral("V", 5), new romanNumeral("IV", 4),
-        new romanNumeral("I", 1)
-    ];
-
-    var arabicToRoman = function(arabic) {
-
-        var result = "";
-
-        romanNumerals.forEach(function(roman) {
-            while (arabic >= roman.value && arabic !== 0) {
-                result += roman.numeral;
-                arabic -= roman.value;
-            }
-        }, this);
-
-        return result;
-    };
-
-    return arabicToRoman(num);
+    return result;
 }
 
-convert(3999);
+convertToRoman(3999);
