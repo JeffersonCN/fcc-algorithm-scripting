7de9fe7029814ea3e12fe084c9b9aae83f913e96
Jefferson Nascimento <jefferson.nascimento3@fatec.sp.gov.br>
2016-07-22 16:05:16 -0300

    Add solution: Boo Who

---
+++ 1. Intermediate Algorithm Scripting/boo-who.js
@@ -0,0 +1,11 @@
+// https://www.freecodecamp.com/challenges/boo-who
+
+function booWho(bool) {
+  // What is the new fad diet for ghost developers? The Boolean.
+  if (typeof bool == "boolean") {
+    return true;
+  }
+  return false;
+}
+
+booWho(null);
