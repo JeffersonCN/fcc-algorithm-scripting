7316c8f17f9a7cce20daa9b93d547df869e97dc3
Jefferson Nascimento <jefferson.nascimento3@fatec.sp.gov.br>
2016-07-22 23:05:02 -0300

    Add solution: Finders Keepers

---
+++ 1. Intermediate Algorithm Scripting/finders-keepers.js
@@ -0,0 +1,9 @@
+// https://www.freecodecamp.com/challenges/finders-keepers
+
+function findElement(arr, func) {
+  var newArr = arr.filter(func);
+  
+  return newArr[0];
+}
+
+findElement([1, 2, 3, 4], function(num){ return num % 2 === 0; });
