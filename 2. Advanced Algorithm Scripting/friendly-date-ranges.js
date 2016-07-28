61ebd6999249ba95fbd94c1e718857b5fccb0bda
Jefferson Nascimento <jefferson.nascimento3@fatec.sp.gov.br>
2016-07-28 20:56:18 -0300

    Add solution: Friendly Date Ranges

---
+++ 2. Advanced Algorithm Scripting/friendly-date-ranges.js
@@ -0,0 +1,87 @@
+// https://www.freecodecamp.com/challenges/friendly-date-ranges
+var monthNames = {
+    0: 'January',
+    1: 'February',
+    2: 'March',
+    3: 'April',
+    4: 'May',
+    5: 'June',
+    6: 'July',
+    7: 'August',
+    8: 'September',
+    9: 'October',
+    10: 'November',
+    11: 'December'
+},
+cardinal = {
+    1: 'st',
+    2: 'nd',
+    3: 'rd'
+};
+
+function getCardinalDay(day) {
+    if (day > 3) {
+        return  day + 'th';
+    } else {
+        return day + cardinal[day];
+    }
+}
+
+function getMonthName(month) {
+    return monthNames[month];
+}
+
+function makeFriendlyDates(arr) {
+    var firstDate = new Date(arr[0].split('-')),
+        lastDate = new Date(arr[1].split(('-'))),
+        currentDate = new Date(Date.now()),
+        currentYear = currentDate.getFullYear(),
+        rangeInSeconds = Math.round((lastDate - firstDate)/1000),
+        years = rangeInSeconds / (60*60*24*365),
+        begin,
+        end,
+        date1,
+        date2,
+        friendlyDates = [];
+
+    begin = {
+        day: getCardinalDay(firstDate.getUTCDate()),
+        month: getMonthName(firstDate.getMonth()),
+        year: firstDate.getFullYear()
+    };
+
+    end = {
+        day: getCardinalDay(lastDate.getUTCDate()),
+        month: getMonthName(lastDate.getMonth()),
+        year: lastDate.getFullYear()
+    };
+
+    date1 = begin.month + ' ' + begin.day + ', ' + begin.year;
+    date2 = end.month + ' ' + end.day + ', ' + end.year;
+
+    if (years < 1) {
+        date2 = date2.replace(', ' + end.year, '');
+
+        if (begin.year === currentYear) {
+            date1 = date1.replace(', ' + begin.year, '');
+        }
+    } else {
+        if (begin.month === end.month) {
+            date2 = date2.replace(', ' + end.year, '').replace(end.month + ' ');
+        }
+    }
+
+    friendlyDates.push(date1);
+    friendlyDates.push(date2);
+
+    // years < 1 ---> não mostra o ano na data final
+
+    // se (ano inicial = ano atual) e (years < 1) ---> não mostra o ano na data inicial
+
+    // se o mês inicial = mês final ---> não mostra ano e mês na data final
+
+    console.log(friendlyDates);
+    return friendlyDates;
+}
+
+makeFriendlyDates(['2016-07-01', '2016-07-04']);
